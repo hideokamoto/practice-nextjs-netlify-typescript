1d2e96e93ee622762e8f651f0088317e6eede448
hideokamoto <hide.okamoto@digitalcube.jp>
2020-01-08 11:53:24 +0900

    replace typescript

--- components/nav.tsx
+++ components/nav.tsx
@@ -5,8 +5,10 @@ const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
 ].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
+  return {
+    ...link,
+    key: `nav-link-${link.href}-${link.label}`
+  }
 })
 
 const Nav = () => (
